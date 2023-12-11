ccbee251fd74685eeb5be3f6911e30378871666a
opoiasnik <122808904+opoiasnik@users.noreply.github.com>
2023-12-11 13:54:20 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,8 +7,8 @@
 #include <netdb.h>
 
 void error(const char *msg) {
-    perror(msg); // Виводить помилку і завершує програму
-    exit(0);
+    perror(msg); // Displays the error and ends the program
+    exit(1); // Changed from exit(0) to exit(1) to indicate an error exit status
 }
 
 int main(int argc, char *argv[]) {
@@ -18,46 +17,55 @@ int main(int argc, char *argv[]) {
     struct hostent *server;
     char buffer[256];
 
+    printf("Starting the client...\n");
+
     if (argc < 3) {
-        fprintf(stderr, "Usage %s hostname port\n", argv[0]);
-        exit(0);
+        fprintf(stderr, "Usage: %s hostname port\n", argv[0]);
+        exit(1); // Error exit status
     }
 
+    // Convert the port number from string to int
     portno = atoi(argv[2]);
+    if (portno <= 0) {
+        fprintf(stderr, "ERROR: Invalid port provided.\n");
+        exit(1);
+    }
+
+    printf("Creating a socket...\n");
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
 
+    printf("Looking up the host '%s'...\n", argv[1]);
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr, "ERROR, no such host\n");
-        exit(0);
+        fprintf(stderr, "ERROR: No such host found.\n");
+        exit(1);
     }
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
     serv_addr.sin_port = htons(portno);
 
+    printf("Connecting to the server at %s on port %d...\n", argv[1], portno);
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
 
-    printf("Please enter the message: ");
+    printf("Successfully connected to the server.\nPlease enter the message to send: ");
     bzero(buffer, 256);
     fgets(buffer, 255, stdin);
 
     n = write(sockfd, buffer, strlen(buffer));
     if (n < 0)
         error("ERROR writing to socket");
+    else
+        fprintf(stderr, "Sent %d bytes to the server.\n", n);
 
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0)
-        error("ERROR reading from socket");
-
-    printf("%s\n", buffer);
-
-    close(sockfd); // Закриває сокет
+    printf("Closing the socket and terminating the client.\n");
+    close(sockfd);
 
     return 0;
 }
