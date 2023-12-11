e9797e4b3c580af666b8ec364a528e748a4dc1ce
opoiasnik <122808904+opoiasnik@users.noreply.github.com>
2023-12-11 13:09:51 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,63 +1,48 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-void error(const char *msg) {
-    perror(msg); // Виводить помилку і завершує програму
-    exit(1);
+void error(char *msg)
+{ //tato funkcia sa vyuziva ked systemove volanie zlyha
+ perror(msg); //vypise spravu o chybe a ukonci program server
+ exit(1);
 }
+int main(int argc, char *argv[])
+{
+ int sockfd, newsockfd, portno, clilen, n; //pomocne premenne
+ char buffer[256]; //buffer pre ulozenie znakov zo socketu
+ struct sockaddr_in serv_addr; //obsahuje adresu servera
+ struct sockaddr_in cli_addr; //obsahuje adresu klienta
+ if (argc < 2) { //kontrola poctu argumentov
+ fprintf(stderr,"ERROR, no port provided\n");
+ exit(1);
+ }
+ //vytvorenie socketu
+ sockfd = socket(PF_INET, SOCK_STREAM, 0);
+ if (sockfd < 0) error("ERROR opening socket");
+ //naplnenie struktury sockaddr_in
+ bzero((char *) &serv_addr, sizeof(serv_addr));
+ portno = atoi(argv[1]);
+ serv_addr.sin_family = AF_INET;
+ serv_addr.sin_addr.s_addr = INADDR_ANY;
+ serv_addr.sin_port = htons(portno);
+ //zviazanie socketu sluzbou bind()
+ if (bind(sockfd, (struct sockaddr *) &serv_addr,
+ sizeof(serv_addr)) < 0)
+ error("ERROR on binding");
 
-int main(int argc, char *argv[]) {
-    int sockfd, newsockfd, portno, clilen, n;
-    char buffer[256];
-    struct sockaddr_in serv_addr, cli_addr;
+ listen(sockfd,5); //vytvorime si front poziadaviek
+ clilen = sizeof(cli_addr); //velkost struktury adresy klienta
+ //akceptovanie spojenia
+ newsockfd = accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
+ if (newsockfd < 0) error("ERROR on accept");
 
-    if (argc < 2) {
-        fprintf(stderr, "ERROR, no port provided\n");
-        exit(1);
-    }
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = atoi(argv[1]);
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR on binding");
-
-    listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-    if (newsockfd < 0)
-        error("ERROR on accept");
-
-    bzero(buffer, 256);
-    n = read(newsockfd, buffer, 255);
-    if (n < 0)
-        error("ERROR reading from socket");
-
-    printf("Here is the message: %s\n", buffer);
-
-    fprintf(stderr, "Received %d bytes\n", n); // Записує статус на stderr
-
-    n = write(newsockfd, "I got your message", 18);
-    if (n < 0)
-        error("ERROR writing to socket");
-
-    close(newsockfd); // Закриває з'єднання з клієнтом
-    close(sockfd); // Закриває сокет
-
-    return 0;
+ bzero(buffer,256); //spracovanie dat od klientov
+ n = read(newsockfd,buffer,255);
+ if (n < 0) error("ERROR reading from socket");
+ printf("Here is the message: %s\n",buffer);
+ n = write(newsockfd,"I got your message",18);
+ if (n < 0) error("ERROR writing to socket");
+ return 0;
 }
